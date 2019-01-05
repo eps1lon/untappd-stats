ca3ef58ec2127c4e80ee3334de914b79779052ee
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2019-01-05 20:26:25 +0100

    feat: improve landing page look

--- src/Body/Landing.tsx
+++ src/Body/Landing.tsx
@@ -1,14 +1,32 @@
 import { Button, TextField, Typography } from "@material-ui/core";
+import { Theme } from "@material-ui/core/styles";
 import { createStyles, makeStyles } from "@material-ui/styles";
 import React from "react";
 import { RouterProps } from "react-router";
 
 import useDocumentTitle from "../useDocumentTitle";
 
-const styles = createStyles({
-  root: {},
-  input: {},
-});
+const styles = (theme: Theme) =>
+  createStyles({
+    root: {
+      alignContent: "center",
+      display: "flex",
+      flexFlow: "column",
+    },
+    button: {
+      margin: "0 auto",
+      marginTop: theme.spacing.unit,
+      width: 150,
+    },
+    title: {
+      margin: "0 auto",
+      marginBottom: 20,
+    },
+    input: {
+      margin: "0 auto",
+      width: 200,
+    },
+  });
 const useClasses = makeStyles(styles);
 
 export interface Props extends RouterProps {}
@@ -43,14 +61,21 @@ function Landing(props: Props) {
 
   return (
     <div className={classes.root}>
-      <Typography variant="h1">Untappd Stats</Typography>
+      <Typography className={classes.title} variant="h1">
+        Untappd Stats
+      </Typography>
       <TextField
         className={classes.input}
         onChange={handleChange}
         onKeyDown={handleKeyDown}
+        label="name of the connoisseur"
         value={name}
       />
-      <Button onClick={handleSearch} variant="contained">
+      <Button
+        className={classes.button}
+        onClick={handleSearch}
+        variant="contained"
+      >
         Open Profile
       </Button>
     </div>
