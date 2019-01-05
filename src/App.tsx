1875d051f85839f38370fa8018a90f462370a3d0
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2019-01-05 14:08:56 +0100

    feat: Move styles from checkboxes to chips in autocomplete

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+import { ThemeProvider } from "@material-ui/styles";
 import React from "react";
 
 import Header from "./Header";
 import Body from "./Body";
 
 export const Context = React.createContext({ version: "unknown" });
+const theme = createMuiTheme();
 
 function App() {
   const context = { version: "0.0.1" };
 
   return (
     <Context.Provider value={context}>
-      <main>
-        <Header />
-        <Body />
-      </main>
+      <ThemeProvider theme={theme}>
+        <main>
+          <Header />
+          <Body />
+        </main>
+      </ThemeProvider>
     </Context.Provider>
   );
 }
--- src/RatingsFilter/BeerStyles.tsx
+++ src/RatingsFilter/BeerStyles.tsx
@@ -1,5 +1,26 @@
-import { Checkbox, FormControlLabel, FormGroup } from "@material-ui/core";
+import {
+  Chip,
+  MenuItem,
+  Paper,
+  TextField,
+  Typography,
+} from "@material-ui/core";
+import { InputBaseComponentProps } from "@material-ui/core/InputBase";
+import { Theme } from "@material-ui/core/styles";
+import { emphasize } from "@material-ui/core/styles/colorManipulator";
+import CancelIcon from "@material-ui/icons/Cancel";
+import { createStyles, makeStyles, useTheme } from "@material-ui/styles";
+import classNames from "classnames";
 import React from "react";
+import Select from "react-select";
+import { ValueContainerProps } from "react-select/lib/components/containers";
+import { MenuProps, NoticeProps } from "react-select/lib/components/Menu";
+import { MultiValueProps } from "react-select/lib/components/MultiValue";
+import { PlaceholderProps } from "react-select/lib/components/Placeholder";
+import { SingleValueProps } from "react-select/lib/components/SingleValue";
+import { OptionProps } from "react-select/lib/components/Option";
+import { ControlProps } from "react-select/lib/components/Control";
+import { SelectComponentsConfig } from "react-select/lib/components";
 
 export type BeerStyle = string;
 export interface Props {
@@ -8,44 +29,244 @@ export interface Props {
   onChange: (beerStyles: BeerStyle[]) => void;
 }
 
+type Option = { value: string; label: string };
+const components: SelectComponentsConfig<Option> = {
+  Control,
+  Menu,
+  MultiValue,
+  NoOptionsMessage,
+  Option,
+  Placeholder,
+  SingleValue,
+  ValueContainer,
+};
+
+const useClasses = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+      height: 250,
+    },
+    input: {
+      display: "flex",
+      padding: 0,
+    },
+    valueContainer: {
+      display: "flex",
+      flexWrap: "wrap",
+      flex: 1,
+      alignItems: "center",
+      overflow: "hidden",
+    },
+    chip: {
+      margin: `${theme.spacing.unit / 2}px ${theme.spacing.unit / 4}px`,
+    },
+    chipFocused: {
+      backgroundColor: emphasize(
+        theme.palette.type === "light"
+          ? theme.palette.grey[300]
+          : theme.palette.grey[700],
+        0.08,
+      ),
+    },
+    noOptionsMessage: {
+      padding: `${theme.spacing.unit}px ${theme.spacing.unit * 2}px`,
+    },
+    singleValue: {
+      fontSize: 16,
+    },
+    placeholder: {
+      position: "absolute",
+      left: 2,
+      fontSize: 16,
+    },
+    paper: {
+      position: "absolute",
+      zIndex: 1,
+      marginTop: theme.spacing.unit,
+      left: 0,
+      right: 0,
+    },
+    divider: {
+      height: theme.spacing.unit * 2,
+    },
+  }),
+);
+
+function beerStyleToOption(beerStyle: BeerStyle): Option {
+  return {
+    label: beerStyle,
+    value: beerStyle,
+  };
+}
+
+function beerStylesToOptions(beerStyles: BeerStyle[]): Option[] {
+  return beerStyles.map(beerStyleToOption);
+}
+
 function BeerStyles(props: Props) {
   const { availableStyles, onChange, selectedStyles } = props;
 
-  const createHandleCheckboxClick = (beerStyle: string) => (
-    event: React.ChangeEvent<{}>,
-  ) => {
-    const { target } = event;
-    if (!(target instanceof HTMLInputElement)) {
-      throw new Error(
-        "handleCheckboxClick was not passed to an HTMLInputElement",
-      );
-    }
-
-    if (target.checked && selectedStyles.indexOf(beerStyle) === -1) {
-      onChange(selectedStyles.concat(beerStyle));
-    } else if (!target.checked) {
-      onChange(
-        selectedStyles.filter((existingStyle) => existingStyle !== beerStyle),
-      );
-    }
+  const classes = useClasses();
+  const theme = useTheme<Theme>();
+  const selectStyles = {
+    input: (base: any) => ({
+      ...base,
+      color: theme.palette.text.primary,
+      "& input": {
+        font: "inherit",
+      },
+    }),
   };
 
+  const handleChange = React.useCallback(
+    (options: Option[] | Option | null | undefined) => {
+      if (options == null) {
+        onChange([]);
+      } else if (!Array.isArray(options)) {
+        onChange([options.value]);
+      } else {
+        onChange(options.map(({ value }) => value));
+      }
+    },
+    [onChange],
+  );
+  const options: Option[] = React.useMemo(
+    () => beerStylesToOptions(availableStyles),
+    [availableStyles],
+  );
+  const value: Option[] = React.useMemo(
+    () => beerStylesToOptions(selectedStyles),
+    [selectStyles],
+  );
+
   return (
-    <FormGroup>
-      {availableStyles.map((beerStyle) => {
-        return (
-          <FormControlLabel
-            key={beerStyle}
-            control={
-              <Checkbox checked={selectedStyles.indexOf(beerStyle) !== -1} />
-            }
-            label={beerStyle}
-            onChange={createHandleCheckboxClick(beerStyle)}
-            value={beerStyle}
-          />
-        );
+    // @ts-ignore
+    <Select<Option>
+      styles={selectStyles}
+      textFieldProps={{
+        label: "Label",
+        InputLabelProps: {
+          shrink: true,
+        },
+      }}
+      options={options}
+      // ClearIndicator<Option> not assignable to ClearIndicator<string>
+      components={components as any}
+      value={value}
+      onChange={handleChange}
+      placeholder="Select multiple styles"
+      isMulti
+      /* selectProps  */
+      classes={classes}
+    />
+  );
+}
+
+function inputComponent({ inputRef, ...props }: InputBaseComponentProps) {
+  return <div ref={inputRef} {...props as any} />;
+}
+
+function Control(props: ControlProps<Option>) {
+  return (
+    <TextField
+      fullWidth
+      InputProps={{
+        inputComponent,
+        inputProps: {
+          className: props.selectProps.classes.input,
+          inputRef: props.innerRef,
+          children: props.children,
+          ...props.innerProps,
+        },
+      }}
+      {...props.selectProps.textFieldProps}
+    />
+  );
+}
+
+function Menu(props: MenuProps<Option>) {
+  return (
+    <Paper
+      square
+      className={props.selectProps.classes.paper}
+      {...props.innerProps}
+    >
+      {props.children}
+    </Paper>
+  );
+}
+
+function MultiValue(props: MultiValueProps<Option>) {
+  return (
+    <Chip
+      tabIndex={-1}
+      label={props.children}
+      className={classNames(props.selectProps.classes.chip, {
+        [props.selectProps.classes.chipFocused]: props.isFocused,
       })}
-    </FormGroup>
+      onDelete={props.removeProps.onClick}
+      deleteIcon={<CancelIcon {...props.removeProps} />}
+    />
+  );
+}
+
+function NoOptionsMessage(props: NoticeProps<Option>) {
+  return (
+    <Typography
+      color="textSecondary"
+      className={props.selectProps.classes.noOptionsMessage}
+      {...props.innerProps}
+    >
+      {props.children}
+    </Typography>
+  );
+}
+
+function Option(props: OptionProps<Option>) {
+  return (
+    <MenuItem
+      buttonRef={props.innerRef}
+      selected={props.isFocused}
+      component="div"
+      style={{
+        fontWeight: props.isSelected ? 500 : 400,
+      }}
+      {...props.innerProps}
+    >
+      {props.children}
+    </MenuItem>
+  );
+}
+
+function Placeholder(props: PlaceholderProps<Option>) {
+  return (
+    <Typography
+      color="textSecondary"
+      className={props.selectProps.classes.placeholder}
+      {...props.innerProps}
+    >
+      {props.children}
+    </Typography>
+  );
+}
+
+function SingleValue(props: SingleValueProps<Option>) {
+  return (
+    <Typography
+      className={props.selectProps.classes.singleValue}
+      {...props.innerProps}
+    >
+      {props.children}
+    </Typography>
+  );
+}
+
+function ValueContainer(props: ValueContainerProps<Option>) {
+  return (
+    <div className={props.selectProps.classes.valueContainer}>
+      {props.children}
+    </div>
   );
 }
 
